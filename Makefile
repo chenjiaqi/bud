021b0a1a7834139039436f2c31f0a9711c81b971
jiaqi.chen <jiaqichen@tcl.com>
2017-03-08 18:46:05 +0800

    d

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 cc = gcc
-SHELL = cmd.exe
+#SHELL = cmd.exe
 
 a.exe:main.o
-	cc  -o a.exe main.o 
+	$(cc) -o a.exe main.o 
 main.o:main.c
-	cc -c -g -o main.o main.c
+	$(cc) -c -g -o main.o main.c
--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 #include"common.h"
+#include<windows.h>
+
+#ifdef _WIN32
+#define SS 1
+#elif 
+#define SS 2
+#endif
+
 
 //#define STR(s) #s
 //#define LOG2(format,...) printf("%s%d:"format,__FUNCTION__,__LINE__,##__VA_ARGS__)
@@ -7,7 +15,8 @@
 int main(int argc, char *argv[])
 {
     LOG("helloworldd%d\n",5);
-    LOG("helloworld");
+    LOG("helloworld\n");
+    LOG("%d",SS);
     return 0;
 
 }
