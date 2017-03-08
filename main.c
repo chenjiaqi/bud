5f54bafa8ece8c45dbfc3ae95b8ca4a28b2cb4ef
chenjiaqi <chenjiaqi77@qq.com>
2017-03-08 22:15:55 +0800

    add exec

--- main.c
+++ main.c
@@ -4,19 +4,30 @@
 #ifdef _WIN32
 #include<windows.h>
 #define SS 1
-#elif 
+#else 
 #define SS 2
 #endif
 
 //#define STR(s) #s
 //#define LOG2(format,...) printf("%s%d:"format,__FUNCTION__,__LINE__,##__VA_ARGS__)
 
+
+
 int main(int argc, char *argv[])
 {
-    LOG("helloworldd%d\n",5);
-    LOG("helloworld\n");
-    LOG("%d",SS);
-    ;
+    FILE * fstream = NULL;
+    char buff[1024];
+    memset(buff,0,sizeof(buff));
+    if(NULL == (fstream = popen("ping www.baidu.com","r")))
+    {
+        fprintf(stderr,"execute command failed");
+        return -1;
+    }
+    //write(fstream,"uname -a",8);
+    while(NULL != fgets(buff,sizeof(buff),fstream))
+    {
+        printf("%s",buff);
+    }
+    pclose(fstream);
     return 0;
-
 }
